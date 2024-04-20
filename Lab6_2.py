4a413d3af98f97af3b9d376e3695327358def2d7
im0nf1re <imonfire.prod@gmail.com>
2024-04-20 11:23:12 +0500

    Рефакторинг

--- Lab6_2.py
+++ Lab6_2.py
@@ -8,7 +8,6 @@ def remove_first_occurrence(tup, value):
         return tup[:index] + tup[index+1:]
     return tup
 
-# Примеры использования функции
 print(remove_first_occurrence((1, 2, 3), 1))
 print(remove_first_occurrence((1, 2, 3, 1, 2, 3, 4, 5, 2, 3, 4, 2, 4, 2), 3))
 print(remove_first_occurrence((2, 4, 6, 6, 4, 2), 9))
